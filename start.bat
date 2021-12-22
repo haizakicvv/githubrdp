26672b7da7bbc61471b3a62a5b5bd21a38a20e29
paraquiet@root~# del hal.dll <74558190+paraquiet@users.noreply.github.com>
2021-12-22 14:07:42 +0200

    @fakeprqt

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2021 By @nnchnl" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2021 By @fakeprqt" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator qwerty_123 /add >nul
 net localgroup administrators administrator /add >nul
@@ -15,8 +15,8 @@ echo  Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @nnchnl
+echo Password: qwerty_123
 echo .
-echo  RDP By @nnchnl
+echo  RDP By @fakeprqt
 echo   Connect to Your RDP !
 ping -n 10 127.0.0.1 >nul
